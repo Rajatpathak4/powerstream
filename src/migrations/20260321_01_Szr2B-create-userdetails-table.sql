65c5dc7c4404ddc1827ac55b818d88a9195763e7
rajat pathak <pathakrajat463@gmail.com>
2026-03-22 22:59:59 +0530

    Crawling weather data done

--- src/migrations/20260321_01_Szr2B-create-userdetails-table.sql
+++
@@ -1,5 +0,0 @@
--- Create userdetails table
--- depends: 
-
-
-ALTER TABLE "users" RENAME TO user_detail;
--- src/migrations/20260322_02_Qqq0x-alter-weather-table.sql
+++
@@ -1,5 +0,0 @@
--- alter weather table
--- depends:
-
-
-alter table weather_data add column if not exists city_id integer;

d7378b720875d900e8e5b50000c076a6837b0797
rajat pathak <pathakrajat463@gmail.com>
2026-03-22 23:02:29 +0530

    Crawling weather data done

--- frontend/src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production : false,
-    FastApiBaseUrl: "http://127.0.0.1:8000",
-    header: "PowerStream",
-    animationClass: "animate animated animate zoomInDown"
-}
